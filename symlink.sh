4896cc2037fa05c9c7ff7216d629028c8b65fd08
Gary-Guo <mingliangguo@gmail.com>
2017-06-28 22:09:37 -0400

    add a symlink.sh script to create symbolic links

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ln -s .vimrc ~/.vimrc
+ln -s .gvimrc ~/.gvimrc
+ln -s .zshrc ~/.zshrc
+ln -s .bashrc ~/.bashrc
+ln -s .bash_profile ~/.bash_profile
+ln -s .myprofile ~/.myprofile
+ln -s .gitconfig ~/.gitconfig
+ln -s .gitignore ~/.gitignore
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .eslintrc ~/.eslintrc
+ln -s .aliases ~/.aliases
