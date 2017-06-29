349ba804c55a52e0bfbd183c809320e97a76b460
Gary Guo <gary.guo@ibm.com>
2017-06-28 22:30:09 -0400

    fix the symb link

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-ln -s .vimrc ~/.vimrc
-ln -s .gvimrc ~/.gvimrc
-ln -s .zshrc ~/.zshrc
-ln -s .bashrc ~/.bashrc
-ln -s .bash_profile ~/.bash_profile
-ln -s .myprofile ~/.myprofile
-ln -s .gitconfig ~/.gitconfig
-ln -s .gitignore ~/.gitignore
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .eslintrc ~/.eslintrc
-ln -s .aliases ~/.aliases
+rm ~/.vimrc
+rm ~/.gvimrc
+rm ~/.zshrc
+rm ~/.bashrc
+rm ~/.bash_profile
+rm ~/.myprofile
+rm ~/.gitconfig
+rm ~/.gitignore
+rm ~/.tmux.conf
+rm ~/.eslintrc
+rm ~/.aliases
+
+cwd=${PWD} 
+ln -fs $cwd/.vimrc ~/.vimrc
+ln -fs $cwd/.gvimrc ~/.gvimrc
+ln -fs $cwd/.zshrc ~/.zshrc
+ln -fs $cwd/.bashrc ~/.bashrc
+ln -fs $cwd/.bash_profile ~/.bash_profile
+ln -fs $cwd/.myprofile ~/.myprofile
+ln -fs $cwd/.gitconfig ~/.gitconfig
+ln -fs $cwd/.gitignore ~/.gitignore
+ln -fs $cwd/.tmux.conf ~/.tmux.conf
+ln -fs $cwd/.eslintrc ~/.eslintrc
+ln -fs $cwd/.aliases ~/.aliases
