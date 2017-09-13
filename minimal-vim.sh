159dd8ae1a598194fb52fdae546883d2e41e6d55
mingliangguo <mingliangguo@gmail.com>
2017-09-13 11:27:28 -0400

    fix folder navigation issues for nerdtree

--- minimal-vim.sh
+++ minimal-vim.sh
@@ -12,10 +12,14 @@ echo "installing pathogen"
 mkdir -p ~/.vim/bundle ~/.vim/autoload
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+echo "installing color themes..."
+mkdir -p ~/.vim/colors
+curl -LSso ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+
 echo "installing sensible.vim..."
 mkdir -p ~/.vim/bundle/plugin
 cd ~/.vim/bundle/plugin
-curl -LSso ~/.vim/bundle/plugin/sensible.vim https://github.com/tpope/vim-sensible/blob/master/plugin/sensible.vim
+curl -LSso ~/.vim/bundle/plugin/sensible.vim  https://raw.githubusercontent.com/tpope/vim-sensible/master/plugin/sensible.vim
 
 echo "installing nerdtree.vim"
 mkdir -p ~/temp/nerdtree
@@ -24,14 +28,26 @@ wget https://github.com/scrooloose/nerdtree/archive/5.0.0.zip
 unzip 5.0.0.zip
 mv nerdtree-5.0.0 nerdtree
 mv nerdtree ~/.vim/bundle/
+cd ~
 rm -rf ~/temp/nerdtree
+
+echo "installing ctrlp.vim"
+mkdir -p ~/temp/ctrlp
+cd ~/temp/ctrlp
+wget https://github.com/ctrlpvim/ctrlp.vim/archive/1.80.zip
+unzip 1.80.zip
+mv  ctrlp.vim-1.80 ctrlp
+mv ctrlp ~/.vim/bundle/
+cd ~
+rm -rf ~/temp/ctrlp
 
 echo "Adding contents to ~/.vimrc"
 echo "set nocompatible
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
-color delek
+\" color delek
+color molokai
 set vb
 let mapleader = \",\"
 set number
@@ -39,14 +55,16 @@ set tabstop=2
 set shiftwidth=2
 set softtabstop=2
 set expandtab
+set hlsearch
+let g:ctrlp_map = '<c-p>'
+let g:ctrlp_cmd = 'CtrlP'
+let NERDTreeDirArrows=0
 :nnoremap <leader>n :NERDTreeToggle<cr>
 \" easier window movement
 :nnoremap <C-H> <C-W>h
 :nnoremap <C-J> <C-W>j
 :nnoremap <C-K> <C-W>k
 :nnoremap <C-L> <C-W>l
-\" compile swift projects
-:nnoremap <leader>B :!clear && swift build<cr>
 " > ~/.vimrc
 
 
