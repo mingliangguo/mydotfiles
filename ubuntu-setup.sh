2e5efe9a135686d9e66ecbe9a9bf2b14f5f7ae49
mingliangguo <mingliangguo@gmail.com>
2017-09-25 10:44:39 -0400

    add neovim

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -3,3 +3,9 @@
 sudo apt-get install wget
 wget https://github.com/gsamokovarov/jump/releases/download/v0.13.0/jump_0.13.0_amd64.deb
 sudo dpkg -i jump_0.13.0_amd64.deb
+
+# install latest nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install neovim
+
