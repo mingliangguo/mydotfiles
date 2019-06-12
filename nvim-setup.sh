d18ba167bc48f205dc2dcac99660340cbdc7abee
Mingliang Guo <gary.guo@ibm.com>
2019-06-12 14:17:39 -0400

    update nvim installation script

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -2,6 +2,9 @@
 # script to install nvim
 # https://github.com/neovim/neovim/wiki/Installing-Neovim
 
+pip3 install --user pynvim
+brew install nvim
+
 mkdir -p ~/.config/nvim/backup
 mkdir -p ~/.config/nvim/swap
 mkdir -p ~/.config/nvim/undo
